c898fbadc959c78c81f21716d0849466609d4f62
Zarreen Davis <zarreen.davis@outlook.com>
2020-10-25 18:02:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+palindrone = str(input("Enter a word and I'll tell you if it's a Palindrone or not! "))
+if str(palindrone) == str(palindrone) [::-1] :
+  print("This word is a Palindrone!")
+else:
+  print("This word is not a Palindrone!")
